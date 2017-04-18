963846ee18cfe75137a20dab875f1b3403b93773
alexrvs <vasyutin4@gmail.com>
2017-04-18 14:15:24 +0300

    omniauth vk

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-require 'omniauth-facebook'
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
            scope: 'public_profile', info_fields: 'id,name,link'
