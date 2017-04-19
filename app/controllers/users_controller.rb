f239aef699cba17d4dece9600fb5eafd4dc9316f
alexrvs <vasyutin4@gmail.com>
2017-04-19 11:39:42 +0300

    design for signup page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def login
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'welcome/index'
-
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'users#login', via: 'get'
   # root  'static_pages#home'
   # match '/help',    to: 'static_pages#help',    via: 'get'
   # match '/about',   to: 'static_pages#about',   via: 'get'
