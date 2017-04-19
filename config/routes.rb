a48b090836de6e0eb32a5804b68a99d710c5d9c3
alexrvs <vasyutin4@gmail.com>
2017-04-19 10:15:32 +0300

    footer template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  # root  'static_pages#home'
+  # match '/help',    to: 'static_pages#help',    via: 'get'
+  # match '/about',   to: 'static_pages#about',   via: 'get'
+  # match '/contact', to: 'static_pages#contact', via: 'get'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/auth/:provider/callback', to: 'sessions#create'
 
