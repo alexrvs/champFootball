2fcd66dbbfbfd59ed65c03c8f40e93ff58fae4db
alexrvs <vasyutin4@gmail.com>
2017-04-20 13:03:38 +0300

    signup finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render 'show'
+      redirect_to @user
     else
       render 'new'
     end
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name,:last_name,:email,:password,:confirm_password)
+    params.require(:user).permit(:first_name,:last_name,:email,:password,:password_confirmation)
   end
 end
