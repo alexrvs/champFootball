6a161e3555efc0ba55bbb469c8874fc6fe8c8e18
alexrvs <vasyutin4@gmail.com>
2017-04-24 17:29:42 +0300

    env facebook

--- Gemfile
+++ Gemfile
@@ -48,6 +48,9 @@ gem 'bcrypt-ruby', '3.1.2'
 # render partial gem
 gem 'rails_layout'
 gem 'pry'
+# Config application.yml
+gem 'figaro'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-   provider :facebook, "1773942389583387", "8785188a22f52c498f95abe9862bf2f8",
+   provider :facebook, ENV['app_id'], ENV['secret'],
             scope: 'email,user_birthday,public_profile',
             info_fields: 'first_name,last_name,gender,birthday,email'
 end
