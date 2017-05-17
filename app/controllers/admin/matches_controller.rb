d1ff1492832664d294dcdb121587341164c4475d
alexrvs <vasyutin4@gmail.com>
2017-05-17 12:41:19 +0300

    search by team and player fixed

--- app/controllers/admin/matches_controller.rb
+++ app/controllers/admin/matches_controller.rb
@@ -3,12 +3,11 @@ class Admin::MatchesController < ApplicationController
   layout 'admin/admin'
 
   def index
-    unless params[:match].nil?
-      if params[:match][:team_name].present? &&  !params[:match][:team_name].blank?
-        @matches = Match.with_team( params[:match][:team_name])
-      elsif params[:match][:player_name].present? && !params[:match][:player_name].blank?
-        @matches = Match.with_player(params[:match][:player_name])
-      end
+    if params[:match].present?
+      @matches = Match.where(nil).order(:id)  # creates an anonymous scope
+      @matches = Match.with_team( params[:match][:team_name]).order(:id) if params[:match][:team_name].present?
+      @matches = Match.with_player(params[:match][:player_name]).order(:id) if params[:match][:player_name].present?
+      @matches = Match.search_by_team_and_player(params[:match][:team_name], params[:match][:player_name]).order(:id) if params[:match][:team_name].present? && params[:match][:player_name]
     else
       @matches = Match.order(:id)
     end
