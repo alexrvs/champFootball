206fe0c240fbab49c605b688978a2ebdfeeebb27
alexrvs <vasyutin4@gmail.com>
2017-05-19 17:37:50 +0300

    edit info admin user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,40 +1,21 @@
 class Admin::UsersController < ApplicationController
 
-  before_action :find_user, only: [:show, :destroy, :update]
+  before_action :find_user, only: [:edit, :destroy, :update]
+
   layout 'admin/admin'
 
   def index
     @users = User.without_admin
   end
 
-  def show
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to :action => 'show', :id => @user.id
-    else
-      render :action => 'new'
-    end
-  end
-
-  def destroy
-    @user.destroy
-  end
-
   def edit
   end
 
   def update
-    if @user.update_attributes(params[:user])
-      redirect_to :action => 'show', :id => @user.id
-    elsif
-    render 'update'
+    if @user.update_attributes(user_params)
+      redirect_to :action => 'index'
+    else
+    render 'edit'
     end
   end
 
